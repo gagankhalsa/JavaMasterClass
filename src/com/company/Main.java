8514ad89ab4c9c2c908fb33ad3c51e831f8dcbb4
gagankhalsa <gagankhalsa@gmail.com>
2017-11-30 15:43:24 -0500

    2nd commit Operators

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,36 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int intvalue=10;
-	byte bytevalue=120;
-	//for byte and short we always do casting
-	byte newbyteval=(byte) (bytevalue/2);
-	long longvalue=50000+10*(intvalue+bytevalue);
-	short shortval=(short)(500+10*(intvalue+bytevalue));
-	System.out.println("long value"+longvalue);
-		System.out.println("Short value"+shortval);
+		int intvalue = 10;
+		byte bytevalue = 120;
+		//for byte and short we always do casting
+		byte newbyteval = (byte) (bytevalue / 2);
+		long longvalue = 50000 + 10 * (intvalue + bytevalue);
+		short shortval = (short) (500 + 10 * (intvalue + bytevalue));
+		System.out.println("long value" + longvalue);
+		System.out.println("Short value" + shortval);
 		//This is java 8 example
+		precedence(20,80);
 
-		boolean isCar=true;
-		boolean wasCar=isCar ? true:false;
-		if (wasCar)
-		{
+		boolean isCar = true;
+		boolean wasCar = isCar ? true : false;
+		if (wasCar) {
 			System.out.print("was car is true");
+		} else {
+			System.out.println("was car is false");
 		}
-		else
+	}
+     public static double precedence(double a,double b){
+
+		double c=(a+b)*25;
+		System.out.println(c);
+		double d=c%40;
+		if (d<=20)
 		{
-			System.out.print("was car is false");
+			System.out.println("Total was over the limit"+d);
 		}
+		return d;
+		}
+
     }
-}
+
---
+++ src/com/company/Methods_example.java
@@ -0,0 +1,53 @@
+package com.company;
+
+/**
+ * Created by GAGAN on 30/11/2017.
+ */
+public class Methods_example {
+    public static void main(String args[]){
+        //displayHighScorePos("ajay",12);
+        //Methods_example obj=new Methods_example();
+        int scoreValue=calculateHighScore(1500);
+                displayHighScorePos("ajay",scoreValue);
+        scoreValue=calculateHighScore(900);
+        displayHighScorePos("bob",scoreValue);
+        scoreValue=calculateHighScore(400);
+        displayHighScorePos("tom",scoreValue);
+         scoreValue=calculateHighScore(50);
+        displayHighScorePos("jot",scoreValue);
+        //obj.calculateHighScore(1);
+    }
+
+
+
+
+    public  static void displayHighScorePos(String name,int highscoreposition){
+       System.out.println(name+ "managed to get posiotin" +" "+highscoreposition);
+    }
+    public static int calculateHighScore(int score){
+        if (score>1000)
+        {
+            //System.out.printl("1");
+        return 1;
+        }
+        else if (score>500&&score<1000)
+        {return 2;
+            //System.out.println("2");
+        }
+        else if (score>100 && score<500)
+        {return 3;
+            //System.out.println("3");
+        }
+
+
+           else
+        {
+            return 4;
+        }
+
+//return -1;
+
+
+    }
+
+}
